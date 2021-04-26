408dac2df0935da0e56a6df9e587f172ef5d0339
Hai Nguyen <hainguyen1120011@gmail.com>
2021-04-26 21:33:45 +0700

    usepytest

---
+++ test_quadratic_equation.py
@@ -0,0 +1,15 @@
+import math
+import quadratic_equation
+
+def test_square():
+    num = 25
+    assert math.sqrt(num) == 5
+
+def test_mul():
+    num = 7
+    assert 7 * 7 == 40
+
+def test_quality():
+    assert 10 == 11
+    
+
