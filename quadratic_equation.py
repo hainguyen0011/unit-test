1c2996617c63dfa2aa4c35e2ee25078b0b8372e0
Hai Nguyen <hainguyen1120011@gmail.com>
2021-04-28 16:29:19 +0700

    modify order of condition

--- quadratic_equation.py
+++ quadratic_equation.py
@@ -25,17 +25,17 @@ def main():
     b = int(input('enter b: '))
     c = int(input('enter c: '))
     delta = delta_calculator(a, b, c)
-    if delta < 0:
-        real_part, imaginary_part = solve(a, b, c, delta)
-        print(
-            'x1 = {0} + {1}i\nx2 = {0} - {1}i'.format(real_part, imaginary_part))
+    if delta > 0:
+        x1, x2 = solve(a, b, c, delta)
+        print('x1 = {0}\nx2 = {1}'.format(x1, x2))
     elif delta == 0:
         x = solve(a, b, c, delta)
         print('x = {0}'.format(x))
     else:
-        x1, x2 = solve(a, b, c, delta)
-        print('x1 = {0}\nx2 = {1}'.format(x1, x2))
+        real_part, imaginary_part = solve(a, b, c, delta)
+        print(
+            'x1 = {0} + {1}i\nx2 = {0} - {1}i'.format(real_part, imaginary_part))
 
 
 if __name__ == 'main':
-    main()
+    main
