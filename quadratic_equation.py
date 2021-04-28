f461b908a5ac31ddb429bcd89d30922abef6583e
Hai Nguyen <hainguyen1120011@gmail.com>
2021-04-28 16:38:08 +0700

    separate main in quadratic_equation.py to display.py'

--- quadratic_equation.py
+++ quadratic_equation.py
@@ -18,24 +18,3 @@ def solve(a, b, c, delta):
         real_part = -b/(2*a)
         imaginary_part = sqrt(-delta)/(2*a)
         return real_part, imaginary_part
-
-
-def main():
-    a = int(input('enter a: '))
-    b = int(input('enter b: '))
-    c = int(input('enter c: '))
-    delta = delta_calculator(a, b, c)
-    if delta > 0:
-        x1, x2 = solve(a, b, c, delta)
-        print('x1 = {0}\nx2 = {1}'.format(x1, x2))
-    elif delta == 0:
-        x = solve(a, b, c, delta)
-        print('x = {0}'.format(x))
-    else:
-        real_part, imaginary_part = solve(a, b, c, delta)
-        print(
-            'x1 = {0} + {1}i\nx2 = {0} - {1}i'.format(real_part, imaginary_part))
-
-
-if __name__ == 'main':
-    main
