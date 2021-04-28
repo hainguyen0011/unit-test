2829b5a7af3368da6c25266f9d367cd68f29b0ef
Hai Nguyen <hainguyen1120011@gmail.com>
2021-04-28 15:59:24 +0700

    add git ignore

---
+++ quadratic_equation.py
@@ -0,0 +1,39 @@
+from math import sqrt
+
+
+def delta_calculator(a, b, c):
+    delta = b*b - 4*a*c
+    return delta
+
+
+def solve(a, b, c, delta):
+    if delta > 0:
+        x1 = (-b + sqrt(delta)) / (2*a)
+        x2 = (-b - sqrt(delta)) / (2*a)
+        return x1, x2
+    elif delta == 0:
+        x = x1 = -b/(2*a)
+        return x
+    else:
+        real_part = -b/(2*a)
+        imaginary_part = sqrt(-delta)/(2*a)
+        return real_part, imaginary_part
+
+
+def main():
+    a = int(input('enter a: '))
+    b = int(input('enter b: '))
+    c = int(input('enter c: '))
+    delta = delta_calculator(a, b, c)
+    if delta < 0:
+        real_part, imaginary_part = solve(a, b, c, delta)
+        print(
+            'x1 = {0} + {1}i\nx2 = {0} - {1}i'.format(real_part, imaginary_part))
+    elif delta == 0:
+        x = solve(a, b, c, delta)
+        print('x = {0}'.format(x))
+    else:
+        x1, x2 = solve(a, b, c, delta)
+        print('x1 = {0}\nx2 = {1}'.format(x1, x2))
+
+main()
--- test_delta_calculator.py
+++ test_delta_calculator.py
@@ -1,15 +1,21 @@
 import math
+import py
 import quadratic_equation
+import pytest
 
+
+@pytest.mark.square
 def test_square():
     num = 25
     assert math.sqrt(num) == 5
 
+
+@pytest.mark.square
 def test_mul():
     num = 7
     assert 7 * 7 == 40
 
+
+@pytest.mark.others
 def test_quality():
     assert 10 == 11
-    
-
